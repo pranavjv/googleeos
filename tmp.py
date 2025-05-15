e5966ffbdceedcff61b3b8b2716ea054363d5265
pranavjv <pranavaidhya@gmail.com>
2025-05-15 00:12:17 +0000

    fig 2 done

--- tmp.py
+++ tmp.py
@@ -41,14 +41,22 @@ import matplotlib.pyplot as plt
 from os import environ
 
 
-gd_directory = f"{environ['RESULTS']}/cifar10-5k/fc-tanh/seed_0/mse/adam/lr_0.0002_beta1_0.9_beta2_0.995_eps_1e-07"
+# gd_directory = f"{environ['RESULTS']}/cifar10-5k/fc-tanh/seed_0/mse/adam/lr_0.0002_beta1_0.9_beta2_0.995_eps_1e-07"
 
-gd_directory = f"{environ['RESULTS']}/cifar10-5k/fc-tanh/seed_0/mse/vadam/lr_0.0002_beta1_0.9_beta2_0.995_eps_1e-07_vbeta3_1.0_vpower_2_vnormgrad_True_vlrcutoff_19.0"
+# gd_directory = f"{environ['RESULTS']}/cifar10-5k/fc-tanh/seed_0/mse/vadam/lr_0.0002_beta1_0.9_beta2_0.995_eps_1e-07_vbeta3_1.0_vpower_2_vnormgrad_True_vlrcutoff_19.0"
 
-gd_train_loss = torch.load(f"{gd_directory}/test_loss_final")
+# gd_train_loss = torch.load(f"{gd_directory}/test_loss_final")
 
-plt.plot(gd_train_loss)
-plt.yscale("log")
-plt.savefig("plot_tmp.png")
+# plt.plot(gd_train_loss)
+# plt.yscale("log")
+# plt.savefig("plot_tmp.png")
+
+vadam_dir = f"{environ['RESULTS']}/cifar10-5k/resnet32/seed_0/mse/vadam/lr_0.002_beta1_0.9_beta2_0.999_eps_1e-07_vbeta3_1.0_vpower_2_vnormgrad_True_vlrcutoff_19.0"
+
+adam_dir = f"{environ['RESULTS']}/cifar10-5k/resnet32/seed_0/mse/adam/lr_0.001_beta1_0.9_beta2_0.999_eps_1e-07"
+
+vadam_train_loss = torch.load(f"{vadam_dir}/train_loss_final")
+vadam_train_acc = torch.load(f"{vadam_dir}/train_acc_final")
+print(vadam_train_loss[-5:])
 
 
